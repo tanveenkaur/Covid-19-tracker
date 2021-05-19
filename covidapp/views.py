7e3b4ebd3ffb2c8b5ac2d831fc748ec22c66d3d2
FIRST_NAME LAST_NAME <tanveenkaurahujaa@gmail.com>
2021-05-20 03:20:50 +0530

    INDIA DATA ADDDED

--- covidapp/views.py
+++ covidapp/views.py
@@ -12,6 +12,11 @@ headers = {
     }
 response = requests.get(url, headers=headers).json()
 
+
+url1 = "https://api.covid19india.org/data.json"
+response1 = requests.get(url1).json()
+
+
 def home(request):
     noofresults = int(response['results'])
     list = []
@@ -42,7 +47,75 @@ def home(request):
         return render(request , "home.html" , context)
 
     else:
+        for i in range(0,noofresults):
+            if ("Afghanistan" == response['response'][i]['country'] ):
+                new = response['response'][i]['cases']['new']
+                active = response['response'][i]['cases']['active']
+                critical = response['response'][i]['cases']['critical']
+                recovered = response['response'][i]['cases']['recovered']
+                total = response['response'][i]['cases']['total']
+                deaths = int(total)-int(active)-int(recovered)
 
 
-        context = {'list' : list,}
+
+
+        context = {'list' : list,'new' : new ,
+        'active':active,
+        'critical' : critical,
+        'recovered' : recovered ,
+        'total' : total ,
+        'deaths' : deaths,
+        'list' : list,
+        'selectedcountry' : "Afghanistan"}
         return render(request , "home.html",context)
+
+
+
+def india(request):
+    no = len(response1["statewise"])
+    list1 = []
+    for i in range(0,no):
+        list1.append(response1["statewise"][i]["state"])
+
+    if request.method == "POST":
+        selectedstate = request.POST["selectedstate"]
+        for i in range(0,no):
+            if (selectedstate == response1['statewise'][i]['state'] ):
+                    new = (response1['statewise'][i]['deltaconfirmed'])
+                    active = int(response1['statewise'][i]['active'])
+                    lastupdatedtime = response1['statewise'][i]['lastupdatedtime']
+                    recovered = int(response1['statewise'][i]['recovered'])
+                    total = response1['statewise'][i]['confirmed']
+                    deaths = response1['statewise'][i]['deaths']
+                    context = {
+                    'new' : new ,
+                    'active':active,
+                    'lastupdatedtime' : lastupdatedtime,
+                    'recovered' : recovered ,
+                    'total' : total ,
+                    'deaths' : deaths,
+                    'list' : list1,
+                    'selectedstate' : selectedstate,
+                    }
+                    return render(request , "india.html" , context)
+
+
+    else:
+        for i in range(0,no):
+            if ("Total" == response1['statewise'][i]['state'] ):
+                new = (response1['statewise'][i]['deltaconfirmed'])
+                active = int(response1['statewise'][i]['active'])
+                lastupdatedtime = response1['statewise'][i]['lastupdatedtime']
+                recovered = int(response1['statewise'][i]['recovered'])
+                total = response1['statewise'][i]['confirmed']
+                deaths = response1['statewise'][i]['deaths']
+                context = {'list' : list,'new' : new ,
+                'active':active,
+                'lastupdatedtime' : lastupdatedtime,
+                'recovered' : recovered ,
+                'total' : total ,
+                'deaths' : deaths,
+                'list' : list1,
+                'selectedstate' : "Total"}
+
+    return render(request , "india.html" , context)
